5768544f27976528dd43fc7798ade44894635efd
Hiliyeh <hiliyeh.bashir@gmail.com>
2022-01-23 15:41:51 +0100

    seller , bidder identity + validation

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -7,6 +7,7 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function createAuction(event, context) {
   const { title } = event.body;
+  const { email } = event.requestContext.authorizer;
   const now = new Date();
   const endDate = new Date();
   endDate.setHours(now.getHours() + 1);
@@ -18,14 +19,15 @@ async function createAuction(event, context) {
     createdAT: now.toISOString(),
     endingAt: endDate.toISOString(),
     highestBid: {
-      amount: 0,
+      amount: 0
     },
+    seller: email
   };
   try {
     await dynamodb
       .put({
         TableName: process.env.AUCTIONS_TABLE_NAME,
-        Item: auction,
+        Item: auction
       })
       .promise();
   } catch (error) {
@@ -35,7 +37,7 @@ async function createAuction(event, context) {
 
   return {
     statusCode: 201,
-    body: JSON.stringify({ auction }),
+    body: JSON.stringify({ auction })
   };
 }
 
