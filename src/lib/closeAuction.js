6b16404af12f885467bd4794b6b5279e890a03bf
Hiliyeh <hiliyeh.bashir@gmail.com>
2022-01-26 18:30:20 +0100

    notification bidder & seller

--- src/lib/closeAuction.js
+++ src/lib/closeAuction.js
@@ -1,6 +1,7 @@
 import AWS from "aws-sdk";
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
+const sqs = new AWS.SQS();
 
 export async function closeAuction(auction) {
   const params = {
@@ -16,5 +17,31 @@ export async function closeAuction(auction) {
   };
 
   const result = await dynamodb.update(params).promise();
-  return result;
+
+  const { title, seller, highestBid } = auction;
+  const { amount, bidder } = highestBid;
+
+  const notifySeller = sqs
+    .sendMessage({
+      QueueUrl: process.env.MAIL_QUEU_URL,
+      MessageBody: JSON.stringify({
+        subject: "Your item has been sold!",
+        recipient: seller,
+        body: `cool ! your item "${title}" has been sold for $${amount}.`
+      })
+    })
+    .promise();
+
+  const notifyBidder = sqs
+    .sendMessage({
+      QueueUrl: process.env.MAIL_QUEU_URL,
+      MessageBody: JSON.stringify({
+        subject: "you won an auction",
+        recipient: bidder,
+        body: `Whar a great deal ! You got yourself a "${title}" for $${amount}`
+      })
+    })
+    .promise();
+
+  return Promise.all([notifySeller, notifyBidder]);
 }
